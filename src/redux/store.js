f47af776e2bad3166098972c646b96cd86d550d4
Giantdefi <cryptonativecoding@gmail.com>
2022-04-20 16:57:54 +0700

    Commit 2 enable Redux Dev toll on production

--- src/redux/store.js
+++ src/redux/store.js
@@ -24,7 +24,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
-  devTools: process.env.NODE_ENV !== 'production',
+ // devTools: process.env.NODE_ENV !== 'production',
   middleware: [thunk],
 });
 
